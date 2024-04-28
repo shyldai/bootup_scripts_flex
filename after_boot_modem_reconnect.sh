8cf2c0f517101c5a72aefd89a845c56566f2757b
Ecem Türe <ecemture17@gmail.com>
2024-04-28 09:53:20 +0200

    Update commands to get IP

--- after_boot_modem_reconnect.sh
+++ after_boot_modem_reconnect.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-sudo insmod /root/Simcom_wwan/simcom_wwan/simcom_wwan.ko
-sudo lsmod
-sudo dmesg | grep simcom
-sudo ifconfig wwan0 up
 
 nmcli networking off
 nmcli networking on
 
-sudo echo -e 'AT+CNMP=2\r' > /dev/ttyUSB2
-sudo echo -e 'AT$QCRMCALL=1,1\r' > /dev/ttyUSB2
+# Run ifconfig command and store the output in a variable
+output=$(ifconfig)
 
-sudo dhclient -1 -v wwan0
+# Function to run dhclient on the specified interface
+run_dhclient() {
+    local interface="$1"
+    sudo dhclient -v "$interface"
+}
 
-sudo echo -e 'AT+CNMP=2\r' > /dev/ttyUSB2
-sudo echo -e 'AT$QCRMCALL=1,1\r' > /dev/ttyUSB2
-
-sudo dhclient -1 -v wwan0
+# Check if usb1 interface is present in the output and if it has 'inet' in the second line after 'usb1:'
+if grep -q "usb1:" <<< "$output" && grep -q "inet" <<< "$(grep -A 2 "usb1:" <<< "$output" | sed -n '2p')"; then
+    echo "Running dhclient on usb1..."
+    run_dhclient "usb1"
+# Check if usb0 interface is present in the output and if it has 'inet' in the second line after 'usb0:'
+elif grep -q "usb0:" <<< "$output" && grep -q "inet" <<< "$(grep -A 2 "usb0:" <<< "$output" | sed -n '2p')"; then
+    echo "Running dhclient on usb0..."
+    run_dhclient "usb0"
+else
+    echo "No relevant interface found"
+fi
